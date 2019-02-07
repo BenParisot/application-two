a66cd1e41705d91ec1121e526bf6b94036ad8ddf
Ben Parisot <me@benparisot.com>
2019-02-07 15:56:13 -0800

    add search parameter link to name on all applications page, get to display in url of applicant detail

--- src/all-applicants.js
+++ src/all-applicants.js
@@ -7,23 +7,31 @@ for(let index = 0; index < applicants.length; index++) {
     const currentApplicant = applicants[index];
 
     const row = document.createElement('tr');
+
+    const link = document.createElement('a');
+    link.target = 'blank';
+    link.href = 'applicant-detail.html?name=' + encodeURIComponent(currentApplicant.firstName);
     const firstName = document.createElement('td');
     const lastName = document.createElement('td');
     const city = document.createElement('td');
     const position = document.createElement('td');
     const salary = document.createElement('td');
 
-    firstName.textContent = currentApplicant.firstName;
+
+    link.textContent = currentApplicant.firstName;
     lastName.textContent = currentApplicant.lastName;
     city.textContent = currentApplicant.city;
     position.textContent = currentApplicant.position;
     salary.textContent = currentApplicant.salary;
+
+    firstName.appendChild(link);
 
     row.appendChild(firstName);
     row.appendChild(lastName);
     row.appendChild(city);
     row.appendChild(position);
     row.appendChild(salary);
+    console.log(firstName);
 
     tableBodyNode.appendChild(row);
 }
