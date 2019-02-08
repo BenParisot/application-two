bdeee3a94a05ebdadd418600d90e88146fcbc882
Ben Parisot <me@benparisot.com>
2019-02-07 16:19:16 -0800

    get search parameter name and find object in array that matches that to firstName

--- src/applicant-detail.js
+++ src/applicant-detail.js
@@ -1,4 +1,5 @@
 let applicants = [];
+let currentApplicant = null;
 
 const applicantsJSON = window.localStorage.getItem('applicants');
 const applicantsArray = JSON.parse(applicantsJSON);
@@ -9,5 +10,13 @@ if(applicantsJSON) {
     }  
 }
 
-console.log(applicants);
-console.log(applicantsJSON);
+const searchParams = new URLSearchParams(window.location.search);
+const queryName = searchParams.get('name');
+
+for(let index = 0; index < applicants.length; index++) {
+    if(queryName === applicants[index].firstName) {
+        currentApplicant = applicants[index];
+    }
+}
+
+console.log(currentApplicant);
