1a7bcba59afd1abf12a93cf0d78e63a39513dbd9
Ben Parisot <me@benparisot.com>
2019-02-07 16:24:52 -0800

    get whole site working end to end

--- src/applicant-detail.js
+++ src/applicant-detail.js
@@ -4,6 +4,8 @@ let currentApplicant = null;
 const applicantsJSON = window.localStorage.getItem('applicants');
 const applicantsArray = JSON.parse(applicantsJSON);
 
+const applicantsTableNode = document.getElementById('applicant-detail');
+
 if(applicantsJSON) {
     for(let index = 0; index < applicantsArray.length; index++) {
         applicants.push(applicantsArray[index]);
@@ -18,5 +20,28 @@ for(let index = 0; index < applicants.length; index++) {
         currentApplicant = applicants[index];
     }
 }
+
+const row = document.createElement('tr');
+const firstName = document.createElement('td');
+const lastName = document.createElement('td');
+const city = document.createElement('td');
+const position = document.createElement('td');
+const salary = document.createElement('td');
+
+
+firstName.textContent = currentApplicant.firstName;
+lastName.textContent = currentApplicant.lastName;
+city.textContent = currentApplicant.city;
+position.textContent = currentApplicant.position;
+salary.textContent = currentApplicant.salary;
+
+
+row.appendChild(firstName);
+row.appendChild(lastName);
+row.appendChild(city);
+row.appendChild(position);
+row.appendChild(salary);
+
+applicantsTableNode.appendChild(row);
 
 console.log(currentApplicant);
